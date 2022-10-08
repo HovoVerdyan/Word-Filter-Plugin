851217007a3aa90f4018358b8cb2909303e6b290
Hovhannes <hovhannesverdyan@gmail.com>
2022-10-08 13:19:51 +0400

    Commitied CSS changes as well as fully ready main settings page

--- wp-content/plugins/Word Filter/Word-filter.php
+++ wp-content/plugins/Word Filter/Word-filter.php
@@ -2,10 +2,54 @@
 
 /*
 Plugin Name: Word Filter
-Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
-Description: A brief description of the Plugin.
+Description: A plugin to filter unwanted the words from posts
 Version: 1.0
-Author: wpfrogs
-Author URI: http://URI_Of_The_Plugin_Author
-License: A "Slug" license name e.g. GPL2
+Author: Hovhannes Verdyan
 */
+
+if ( ! defined( 'ABSPATH' ) ) exit; //Exit of accessed directly
+
+class WordFilter {
+
+	public function __construct()
+	{
+		add_action('admin_menu', array($this, 'ourMenu'));
+	}
+
+	public function ourMenu()
+	{
+		$mainPageHook = add_menu_page('Words to filter', 'Word Filter', 'manage_options', 'wordfilter', array($this, 'wordFilterPage'), 'dashicons-megaphone', 4);
+        add_submenu_page('wordfilter', 'Words to filter', 'Words List', 'manage_options', 'wordfilter', array($this, 'wordFilterPage'));
+        add_submenu_page('wordfilter', 'Word Filter Options', 'Options', 'manage_options', 'word-filter-options', array($this, 'wordFilterOptions'));
+        add_action("load-{$mainPageHook}", array($this, 'mainPageAssets'));
+	}
+
+    public function mainPageAssets()
+    {
+        wp_enqueue_style('filterAdminCSS', plugin_dir_url(__FILE__) . 'style.css');
+    }
+
+	public function wordFilterPage()
+	{ ?>
+		<div class="wrap">
+            <h1>Word Filter</h1>
+            <form method="POST">
+                <label for="plugin_words_to_filter"><p>Enter comma separated words to filter</p></label>
+                <div class="word-filter__flex-container">
+                    <textarea name="plugin_words_to_filter" placeholder="bed, pit, slow, fast..."></textarea>
+                </div>
+                <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes">
+            </form>
+        </div>
+
+	<?php }
+
+    public function wordFilterOptions()
+	{ ?>
+		Hello World Second TIme!
+
+	<?php }
+
+}
+
+$wordFilter = new WordFilter();
